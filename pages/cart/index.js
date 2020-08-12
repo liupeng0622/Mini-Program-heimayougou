65831afc6274e9c0c303cc3087efa452872c1bd9
liupeng0622 <liupeng0622@qq.com>
2020-08-12 20:03:27 +0800

    购物车

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,54 @@
-// pages/cart/index.js
+/*
+1. 获取用户收货地址
+  1> 绑定点击事件
+  2> 调用小程序内置 api，获取用户的收货地址
+
+  3> 获取用户对小程序所授予获取地址的权限状态 scope
+    1> 如果用户点击获取地址的提示框为确定，scope 值为 true 
+    authSetting: scope.address: true
+    2> 如果用户从来没有调用过 api，值为 undefined
+    3> 如果用户在提示框点了取消，scope 值为 false
+      诱导用户自己打开授权设置页面 wx.openSetting，当用户重新给予获取地址权限的时候，获取收货地址
+    4> 把获取到的收货地址存入到本地存储中
+2. 页面加载完毕
+  0> onLoad onShow
+  1> 获取本地存储中的地址数据
+  2> 把这个数据设置给 data 中的一个变量
+      */
+
+import { getSetting, chooseAddress, openSetting } from "../../utils/asyncWx.js"
+import regeneratorRuntime from '../../lib/runtime/runtime'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    address: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onShow(){
+    // 1. 获取缓存中收获地址信息
+    const address = wx.getStorageSync("address");;
+    // 2. 给 data 赋值
+    this.setData({
+      address
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 点击收货地址
+  async handleChooseAddress(){
+    try {
+      // 1. 获取权限状态
+      const res1 = await getSetting();
+      const scopeAddress = res1.authSetting["scope.address"]; 
+      // 2. 判断权限状态
+      if(scopeAddress === false){
+        // 诱导用户打开授予权限页面
+        await openSetting();
+      };
+      // 调用获取地址api
+      let address = await chooseAddress();
+      address.all = address.provinceName+address.cityName+address.countyName+address.detailInfo;
+      // 存入到缓存中
+      wx.setStorageSync("address", address);
+    } catch (error) {
+      console.log(error);
+    }
   }
 })
